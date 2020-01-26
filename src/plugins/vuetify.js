fbe48be3441684098f175223631a507d3605587e
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-01-26 11:36:16 +0800

    Added premade layout.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,32 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+// import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    rtl: false,
+    theme: {
+        dark: true,
+        themes: {
+            dark: {
+                primary: '#21CFF3',
+                accent: '#FF4081',
+                secondary: '#ffe18d',
+                success: '#4CAF50',
+                info: '#2196F3',
+                warning: '#FB8C00',
+                error: '#FF5252'
+            },
+            light: {
+                primary: '#1976D2',
+                accent: '#e91e63',
+                secondary: '#30b1dc',
+                success: '#4CAF50',
+                info: '#2196F3',
+                warning: '#FB8C00',
+                error: '#FF5252'
+            }
+        }
+    }
 });
