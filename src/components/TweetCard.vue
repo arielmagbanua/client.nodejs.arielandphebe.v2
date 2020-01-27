521d3f9566b229a8b793e3b489b9741fcd32bfaa
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-01-27 15:25:53 +0800

    Fixed the tweet ids issues and finalized the tweet card properties.

--- src/components/TweetCard.vue
+++ src/components/TweetCard.vue
@@ -1,8 +1,10 @@
 <template>
-  <v-card class="mx-auto" color="#26c6da" dark max-width="700" min-width="400">
+  <v-card class="ma-2" color="#26c6da" dark>
     <v-card-title>
       <v-icon large left>mdi-twitter</v-icon>
-      <span class="title font-weight-light">@{{ username }}</span>
+      <span class="title font-weight-light">
+        <a class="tweet-profile" :href="owner.profile_url" target="_blank">@{{ owner.screen_name }}</a>
+      </span>
     </v-card-title>
 
     <v-card-text class="headline font-weight-bold">"{{ text }}"</v-card-text>
@@ -12,24 +14,26 @@
         <v-list-item-avatar color="grey darken-3">
           <v-img
             class="elevation-6"
-            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+            :src="owner.profile_image_url"
           ></v-img>
         </v-list-item-avatar>
 
         <v-list-item-content>
-          <v-list-item-title>{{ user }}</v-list-item-title>
+          <v-list-item-title>
+            <a class="tweet-profile" :href="owner.profile_url" target="_blank">{{ owner.name }}</a>
+          </v-list-item-title>
         </v-list-item-content>
 
         <v-row align="center" justify="end">
           <a class="like-tweet" :href="'https://twitter.com/intent/like?tweet_id=' + id">
             <v-icon class="mr-1">mdi-heart</v-icon>
-            <span class="subheading mr-2">{{ likes }}</span>
+            <!-- <span class="subheading mr-2">{{ likes }}</span> -->
           </a>
 
           <a class="share-tweet" :href="'https://twitter.com/intent/retweet?tweet_id=' + id">
             <span class="mr-1">·</span>
             <v-icon class="mr-1">mdi-share-variant</v-icon>
-            <span class="subheading">{{ shares }}</span>
+            <!-- <span class="subheading">{{ shares }}</span> -->
           </a>
         </v-row>
       </v-list-item>
@@ -40,7 +44,8 @@
 <style lang="scss">
 a {
   &.like-tweet,
-  &.share-tweet {
+  &.share-tweet,
+  &.tweet-profile {
     text-decoration: none;
     color: white !important;
   }
@@ -52,10 +57,10 @@ export default {
   props: {
     id: String,
     text: String,
-    user: String,
-    username: String,
-    likes: Number,
-    shares: Number
+    owner: Object,
+    media: Array
+    // likes: Number,
+    // shares: Number
   },
 
   data: () => ({
--- src/views/Twitter.vue
+++ src/views/Twitter.vue
@@ -1,38 +1,100 @@
 <template>
   <section class="twitter">
-    <v-container class="fill-height">
+    <v-container fluid>
       <v-row dense>
-        <tweet-card 
-              id="507185938620219395"
-              text="Sample Content"
-              user="Evan You"
-              username="evan_you"
-              likes="123"
-              shares="456"
-            />
+        <v-col
+          v-for="tweet in tweets"
+          :key="tweet.post_id"
+          :cols="tweet.flex"
+        >
+          <tweet-card
+            :id="tweet.id"
+            :text="tweet.text"
+            :owner="tweet.owner"
+          />
+        </v-col>
       </v-row>
     </v-container>
-    <!-- <tweet-card></tweet-card> -->
   </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import TweetCard from '@/components/TweetCard';
+  // @ is an alias to /src
+  import TweetCard from '@/components/TweetCard';
 
-export default {
-  name: 'home',
-  components: {
-    'tweet-card': TweetCard
-  },
-  data() {
-    return {
-      tweets: []
+  import firebase from 'firebase/app';
+  // Required for side-effects
+  require("firebase/firestore");
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDID0n5CRP77yMMB4SyqDRwgY4hMzv2Pvo",
+    authDomain: "arielandphebe.firebaseapp.com",
+    databaseURL: "https://arielandphebe.firebaseio.com",
+    projectId: "arielandphebe",
+    storageBucket: "arielandphebe.appspot.com",
+    messagingSenderId: "436648050769",
+    appId: "1:436648050769:web:25ba8af9badea69441b870",
+    measurementId: "G-2DL2C0PRPX"
+  };
+
+  // Initialize Cloud Firestore through Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const firestore = firebase.firestore();
+  const tweetCollection = firestore.collection('tweets');
+
+  export default {
+    name: 'home',
+    components: {
+      'tweet-card': TweetCard
+    },
+    data() {
+      return {
+        tweets: []
+      }
+    },
+    
+    mounted() {
+      tweetCollection.get()
+        .then((querySnapshot) => {
+          const tweetPromises = querySnapshot.docs.map(async (doc) => {
+            const data = doc.data();
+            
+            // query first the owner doc
+            const owner = await data.owner.get();
+
+            return await {
+              id: data.post_id.toString(),
+              text: data.text,
+              media: data.media,
+              owner: owner.data()
+            }
+          });
+
+          Promise.all(tweetPromises)
+            .then((tweets) => {
+              // this.tweets = tweets;
+              this.tweets = tweets.map((tweet, index) => {
+                let cardFlexWidth = 3
+
+                if (index <= 1) {
+                  cardFlexWidth = 6;
+                }
+
+                // resize for special case where tweet is too long
+                if (cardFlexWidth === 3 && tweet.text.length > 100) {
+                  cardFlexWidth = 6;
+                }
+                
+                tweet.flex = cardFlexWidth;
+
+                return tweet;
+              });
+            })
+            .catch((err) => {
+              console.error(err);
+            });
+        });
     }
-  },
-  
-  mounted() {
-
   }
-}
 </script>
