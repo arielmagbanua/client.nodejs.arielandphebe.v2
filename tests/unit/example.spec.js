d2e12ed4661a444cfe71dc5d652145af042f73fa
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-01-29 00:03:15 +0800

    Added and fixed the jest unit test.

---
+++ tests/unit/example.spec.js
@@ -0,0 +1,41 @@
+// Libraries
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify);
+
+// Utilities
+import {
+  mount,
+  createLocalVue
+} from '@vue/test-utils'
+
+// import { shallowMount } from '@vue/test-utils'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+// describe('HelloWorld.vue', () => {
+//   it('Welcome Message Test', () => {
+//     const msg = 'Welcome to Vuetify';
+//     const wrapper = shallowMount(HelloWorld, {});
+//     expect(wrapper.text()).toMatch(msg)
+//   })
+// })
+
+const localVue = createLocalVue();
+
+describe('HelloWorld.vue', () => {
+  let vuetify;
+
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+
+  it('Welcome Message Test', () => {
+    const msg = 'Welcome to Vuetify';
+    const wrapper = mount(HelloWorld, {
+      localVue,
+      vuetify
+    });
+    expect(wrapper.text()).toMatch(msg)
+  });
+});
